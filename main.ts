934c6731d5ed5415ff41c958a502a92324d718a2
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2020-12-30 14:06:15 +0100

    Aktualisiere pxt.json, main.blocks, main.ts, anleitung.md

--- main.ts
+++ main.ts
@@ -1,18 +1,19 @@
-let MaximalerWinkel = 0
+let PunktY = 0
+let PunktX = 0
 let LageY = 0
 let LageX = 0
+let MaximalerWinkel = 10
 serial.redirectToUSB()
 basic.forever(function () {
-    MaximalerWinkel = 10
     LageX = input.rotation(Rotation.Roll)
     LageY = input.rotation(Rotation.Pitch)
     LageX = Math.constrain(LageX, MaximalerWinkel * -1, MaximalerWinkel)
     LageY = Math.constrain(LageY, MaximalerWinkel * -1, MaximalerWinkel)
     console.logValue("LageX", LageX)
 console.logValue("LageY", LageY)
-LageX = Math.map(LageX, MaximalerWinkel * -1, MaximalerWinkel, 0, 4)
-    LageY = Math.map(LageY, MaximalerWinkel * -1, MaximalerWinkel, 0, 4)
+PunktX = Math.map(LageX, MaximalerWinkel * -1, MaximalerWinkel, 4, 0)
+    PunktY = Math.map(LageY, MaximalerWinkel * -1, MaximalerWinkel, 4, 0)
     basic.clearScreen()
-    led.plot(LageX, LageY)
+    led.plot(PunktX, PunktY)
     basic.pause(500)
 })
