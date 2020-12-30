fc819dfe2e32294ebd61c812f4069d6b9ab05560
Raik Andritschke <58722460+CalliTGS3@users.noreply.github.com>
2020-12-30 11:26:28 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,16 @@
 let MaximalerWinkel = 0
-let LageX = 0
 let LageY = 0
-serial.redirect(
-SerialPin.USB_TX,
-SerialPin.USB_RX,
-BaudRate.BaudRate9600
-)
+let LageX = 0
+serial.redirectToUSB()
 basic.forever(function () {
     MaximalerWinkel = 10
     LageX = input.rotation(Rotation.Roll)
     LageY = input.rotation(Rotation.Pitch)
     LageX = Math.constrain(LageX, MaximalerWinkel * -1, MaximalerWinkel)
     LageY = Math.constrain(LageY, MaximalerWinkel * -1, MaximalerWinkel)
-    serial.writeLine("" + LageX.toString() + ":" + LageY.toString())
-    LageX = Math.map(LageX, MaximalerWinkel * -1, MaximalerWinkel, 0, 4)
+    console.logValue("LageX", LageX)
+console.logValue("LageY", LageY)
+LageX = Math.map(LageX, MaximalerWinkel * -1, MaximalerWinkel, 0, 4)
     LageY = Math.map(LageY, MaximalerWinkel * -1, MaximalerWinkel, 0, 4)
     basic.clearScreen()
     led.plot(LageX, LageY)
